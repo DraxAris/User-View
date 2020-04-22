2bc9322634efd2c955e36d4e34daf05ffccc2632
Arvid Dezittere <43497237+DraxAris@users.noreply.github.com>
2020-04-22 17:46:52 +0200

    Delete worker.js

--- JS/worker.js
+++
@@ -1,7 +0,0 @@
-//JQUERY needed
-wasModified = false;
-$('#body').click(function(){
-    wasModified = true;
-    //in case click mean it was modified and you should notify user
-    alert('Page was modified');
-});
